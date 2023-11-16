20084aa37476b04da9fe178dfb02a7dccbcfed40
Misael Sandoval <msandoval131@outlook.com>
2023-11-16 13:39:00 -0700

    initial speech recognition test

--- speech2txt.py
+++ speech2txt.py
@@ -23,36 +23,36 @@ elif user_choice == '2':
     lang = 'French'
 
 print(f'I am currently set to translate from English to {lang}')
-## based on https://www.geeksforgeeks.org/python-convert-speech-to-text-and-text-to-speech/
-
+## from https://www.geeksforgeeks.org/python-convert-speech-to-text-and-text-to-speech/
 #speech recognizer 
 r = sr.Recognizer()
 
-def SpeakText(captured_speech):
-    speech_engine = pyttsx3.init()
-    speech_engine.say(captured_speech)
-    speech_engine.runAndWait()
+def SpeakText(command):
+    engine = pyttsx3.init()
+    engine.say(command)
+    engine.runAndWait()
 
 record = '0'
 
-# user begins recording. 
 record = input("press 1 to record")
 
 while(record == '1'):
     try: 
-        print("recording for 3 seconds")
-        time.sleep(1)
-        print("recording..")
-        time.sleep(1)
-        print("recording.. ")
-        time.sleep(1)
+        with sr.Microphone() as source2: 
+            r.adjust_for_ambient_noise(source2, duration=0.2)
+            audio2 = r.listen(source2)
+
+            myText = r.recognize_google(audio2)
+            myText = myText.lower()
+
+            print("Did you say ",myText)
+            SpeakText(myText)
+
         print("done, setting record to 0. ")
         record = '0'
-    except Exception as e: 
-        print("Error message here when speech recognition fails.")
+    except sr.RequestError as e: 
+        print("Could not request results; {0}".format(e))
+    except sr.UnknownValueError: 
+            print("an unknown error occurred.")
     
-
-
-
-
 
