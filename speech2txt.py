43de1fe417d26ea6fa7eb1d9105895133adda33e
Misael Sandoval <msandoval131@outlook.com>
2023-11-16 13:15:05 -0700

    speech to text setup

--- speech2txt.py
+++ speech2txt.py
@@ -2,6 +2,9 @@ import sys
 import keyboard
 import time
 
+import speech_recognition as sr
+import pyttsx3
+
 #usage python3 speech2txt.py <language>
 
 lang = 'English'
@@ -20,9 +23,36 @@ elif user_choice == '2':
     lang = 'French'
 
 print(f'I am currently set to translate from English to {lang}')
+## based on https://www.geeksforgeeks.org/python-convert-speech-to-text-and-text-to-speech/
 
-## runs the loop until q is pressed. Will be used to set microphone to record 
-while not keyboard.is_pressed('q'): 
-    print("press q to quit")
-    time.sleep(5)
+#speech recognizer 
+r = sr.Recognizer()
+
+def SpeakText(captured_speech):
+    speech_engine = pyttsx3.init()
+    speech_engine.say(captured_speech)
+    speech_engine.runAndWait()
+
+record = '0'
+
+# user begins recording. 
+record = input("press 1 to record")
+
+while(record == '1'):
+    try: 
+        print("recording for 3 seconds")
+        time.sleep(1)
+        print("recording..")
+        time.sleep(1)
+        print("recording.. ")
+        time.sleep(1)
+        print("done, setting record to 0. ")
+        record = '0'
+    except Exception as e: 
+        print("Error message here when speech recognition fails.")
+    
+
+
+
+
 
