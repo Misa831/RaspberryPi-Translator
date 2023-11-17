dbea5274b1e2c4ed8a751a711e93b0a9ef41ef83
Misael Sandoval <msandoval131@outlook.com>
2023-11-16 22:41:12 -0700

    multilanguage support

--- speech2txt.py
+++ speech2txt.py
@@ -1,26 +1,37 @@
 
 import speech_recognition as sr
 import pyttsx3
+import sys
 
-#usage python3 speech2txt.py <language>
 
+# sets default language
 lang = 'English'
 
-
 print("Hello! This is the Raspberry Pi Language Translator")
-
-print("please choose a language to translate to: ")
-user_choice = input("[0]: English   [1] Spanish   [2] French   [3] More options\n")
+print("Please choose a language to translate to: ")
+user_choice = input("[0]: English   [1] Spanish   [2] French   [3] More options   [q] Quit Program \n")
 
 if user_choice == '3': 
-    user_choice = input("[5] Portuguese   [6] German   [7] More options\n")
+    user_choice = input("[4] Portuguese   [5] German   [q] Quit Program \n")
+    #lang won't update unless nested 
+    if user_choice == '4':
+        lang = 'Portuguese'
+    elif user_choice == '5':
+        lang = 'German'
+    elif user_choice == 'q':
+        sys.exit()
 elif user_choice == '1':
     lang = 'Spanish'
 elif user_choice == '2':
     lang = 'French'
+elif user_choice == '0':
+    lang = 'English'
+elif user_choice == 'q':
+    sys.exit()
+
 
 print(f'I am currently set to translate from English to {lang}')
-## from https://www.geeksforgeeks.org/python-convert-speech-to-text-and-text-to-speech/
+
 #speech recognizer 
 r = sr.Recognizer()
 
@@ -30,15 +41,15 @@ def SpeakText(command):
     engine.runAndWait()
 
 record = '0'
+# record = input("Press 1 to record\n")
 
-record = input("press 1 to record")
-
+## commented out for testing setup. 
+'''
 while(record == '1'):
     try: 
         with sr.Microphone() as source2: 
             r.adjust_for_ambient_noise(source2, duration=0.2)
             audio2 = r.listen(source2)
-
             myText = r.recognize_google(audio2)
             myText = myText.lower()
 
@@ -51,5 +62,4 @@ while(record == '1'):
         print("Could not request results; {0}".format(e))
     except sr.UnknownValueError: 
             print("an unknown error occurred.")
-    
-
+'''
