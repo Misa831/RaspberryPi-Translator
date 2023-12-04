b0d8748ac4bdfd6b3ca33771f647c8d1f5c93c91
Misael Sandoval <msandoval131@outlook.com>
2023-12-03 23:49:12 -0700

    adds ability to close server.

--- server.py
+++ server.py
@@ -1,20 +1,10 @@
-## This will be the main server file. 
-
-## todo: fix server termination, stays open until another client refreshes the thread. 
-
-## todo: add translation methods here
-
-## keep track of connections and relay messages to each other. 
-
-
-
 import socket     
 import threading
 from text_translation import *
 
 def handle_client(client_socket, client_address):
        print(f"Accepted connection from {client_address}")
-
+       
        while True:
               data = client_socket.recv(1024)
               msg = data.decode('utf-8')
@@ -27,7 +17,7 @@ def handle_client(client_socket, client_address):
               for other_client in clients: 
                      if other_client != client_socket: 
                             try:
-                                   send_string = f"\nReceived message: {tran_msg}\n Press r to begin recording, or 'exit' to quit:"
+                                   send_string = f"\nReceived message: {tran_msg}\nPress r to begin recording, or 'exit' to quit:"
                                    other_client.send(send_string.encode('utf-8'))
                             except socket.error:
                                    quit = input("Press q to quit")
@@ -39,18 +29,22 @@ def handle_client(client_socket, client_address):
 
 
 def start_server():
+       PORT_NUM = 12345
        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-       server_socket.bind(('0.0.0.0', 12345))
+       server_socket.bind(('0.0.0.0', PORT_NUM))
        server_socket.listen(1)
 
-       print("Server Listening on port 12345")
+       print(f"Server Listening on port {PORT_NUM}")
 
        while True:
               client_socket, address = server_socket.accept()
               clients.append(client_socket)
               client_handler = threading.Thread(target = handle_client, args=(client_socket, address))
               client_handler.start()
-
+              exit_handler = input("type 'exit' to quit program: ")
+              if exit_handler.lower() == 'exit':
+                     client_socket.close()
+                     exit()
 clients = []
 start_server()
 
