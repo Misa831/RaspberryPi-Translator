d8d4d10969e8408f67557eadc31c15d86c4330e9
aalhum2 <150925179+aalhum2@users.noreply.github.com>
2023-12-04 22:04:53 -0700

    Updated text_translation.py

--- text_translation.py
+++ text_translation.py
@@ -2,9 +2,9 @@ from googletrans import Translator, constants
 from pprint import pprint
 
 
-def translate_speech(speech):
+def translate_speech(speech, src_lang, dest_lang):
     translator = Translator()
-    translation = translator.translate(speech, src='en', dest = 'es')
+    translation = translator.translate(speech, src=src_lang, dest = dest_lang)
     print(f'{translation.origin}({translation.src}) translates to: {translation.text} ({translation.dest})')
     translated_speech = f'{translation.text}'
-    return translated_speech
+    return translation_speech
